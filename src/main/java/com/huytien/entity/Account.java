c4e95f4245b2fe23ed7374a343f0b1d65c2679fa
huytien6495 <huytien6495@gmail.com>
2023-02-17 02:02:49 +0700

    update resources, entity, pom.xml

---
+++ src/main/java/com/huytien/entity/Account.java
@@ -0,0 +1,58 @@
+package com.huytien.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Formula;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "`Account`")
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "username", length = 50, unique = true, nullable = false, updatable = false)
+    private String username;
+
+    @Column(name = "`password`", length = 200, nullable = false)
+    private String password;
+
+    @Column(name = "first_name", length = 50, nullable = false, updatable = false)
+    private String firstName;
+
+    @Column(name = "first_name", length = 50, nullable = false, updatable = false)
+    private String lastName;
+
+    @Formula("concat(first_name, ' ', last_name)")
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
+    // config enum
+    public enum Role {
+        ADMIN, EMPLOYEE, MANAGER;
+    }
+    // điều kiện ràng buộc trc khi update DB
+    @PrePersist
+    public void prePersist(){
+        if (role == null){
+            role = Role.EMPLOYEE;
+        }
+
+//        if (password == null) {
+//            password = new BCryptPasswordEncoder().encode("123456");
+//        } else {
+//            password = new BCryptPasswordEncoder().encode(password);
+//        }
+    }
+}
